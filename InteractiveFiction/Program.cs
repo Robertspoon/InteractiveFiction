56d372f8c87ec9fff084456d5c91168e18abb79f
Robertspoon <robertthemcjug@gmail.com>
2022-11-21 10:40:18 -0400

    Initial commit

---
+++ InteractiveFiction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveFiction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interactive Fiction");
+
+            Console.ReadKey(true);
+        }
+    }
+}
