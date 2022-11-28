4e5581d1bdb3b069d9d9969eea5aa87d0f703667
Robertspoon <robertthemcjug@gmail.com>
2022-11-28 12:05:54 -0400

    some progress

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -3,26 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace InteractiveFiction
 {
     internal class Program
     {
-        //static string[] story = System.IO.File.ReadAllLines("Story.txt");
+        static string[] story = File.ReadAllLines("Story.txt");
 
+
+
+        static bool gameOver = false;
+
+
+        
         
        
 
         static void Main(string[] args)
         {
 
-
+            
 
             
 
             
 
             Console.ReadKey(true);
+        }
+        static void ShowCurrPage()
+        {
+            for (int currPageNum = 0; currPageNum < story.Length; currPageNum++)
+            {
+                Console.WriteLine(story[currPageNum]);
+
+            }
+
+
         }
     }
 }
