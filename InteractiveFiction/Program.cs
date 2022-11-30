40571e10fa4edab6c70f67858fb1834998b24fb7
Robertspoon <robertthemcjug@gmail.com>
2022-11-30 09:59:35 -0400

    put ShowCurrPage into method

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -11,11 +11,11 @@ namespace InteractiveFiction
     {
         static string story = File.ReadAllText("Story.txt"); //reads story from text file
         static string[] words = story.Split(';');//splits story at ";" point
-        static string[] storyPages = story.Split('%');
+        static string[] storyPages = story.Split('%');//splits story into pages by "%"
         static int currPageNum = 1;
 
-        static bool gameOver = false;
-        static bool storyLoop = true;
+        //static bool gameOver = false;
+        static bool storyLoop = true;//Loops through story allowing gameplay
 
 
         
@@ -24,59 +24,53 @@ namespace InteractiveFiction
 
         static void Main(string[] args)
         {
-
-          while(storyLoop ==true)
-          {
-
-
-            Console.WriteLine(storyPages[currPageNum].Split(';')[0]);
-            Console.WriteLine();
-            Console.WriteLine(storyPages[currPageNum].Split(';')[1]);
-
-            if (storyPages[currPageNum].Split(';').GetLength(0) > 2)
+            while(storyLoop ==true)
             {
-              Console.WriteLine(storyPages[currPageNum].Split(';')[2]);
-
-              ConsoleKeyInfo input;
-               input = Console.ReadKey(true);
-
-              if(input.KeyChar == 'a')
-              {
-               currPageNum = Int32.Parse(storyPages[currPageNum].Split(';')[3]); 
-              }
-              if (input.KeyChar == 'b')
-              {
-                currPageNum = Int32.Parse(storyPages[currPageNum].Split(';')[4]);
-              }
-
-            }
-            else
-            {
-                 Console.ReadKey(true);
-                    storyLoop = false;
+                ShowCurrPage();
             }
 
-
-          }
-
-            
 
             
 
            Console.ReadKey(true);
         }
-        static void ShowCurrPage()
+        static void ShowCurrPage()//Shows the current page and allows you to go through story
         {
-
-            //Console.WriteLine(story.Split(';').Length);
-
-
-        }
+           
+            
 
 
+                Console.WriteLine(storyPages[currPageNum].Split(';')[0]);
+                Console.WriteLine();
+                Console.WriteLine(storyPages[currPageNum].Split(';')[1]);
+
+                if (storyPages[currPageNum].Split(';').GetLength(0) > 2)
+                {
+                    Console.WriteLine(storyPages[currPageNum].Split(';')[2]);
+
+                    ConsoleKeyInfo input;
+                    input = Console.ReadKey(true);
+
+                    if (input.KeyChar == 'a')
+                    {
+                        currPageNum = Int32.Parse(storyPages[currPageNum].Split(';')[3]);
+                    }
+                    if (input.KeyChar == 'b')
+                    {
+                        currPageNum = Int32.Parse(storyPages[currPageNum].Split(';')[4]);
+                    }
+
+                }
+                else
+                {
+                    Console.ReadKey(true);
+                    storyLoop = false;
+                }
+
+            
+        } 
         static void MainMenu()
         {
-
 
         }
     }
